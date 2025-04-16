9babb2fad05f43ae21415b98f8a8c7939f3dda04
avery <hangkai2@illinois.edu>
2025-04-16 03:39:13 -0500

    feat: script update

--- slurm/job_configs/train_2xs_mar.sh
+++ slurm/job_configs/train_2xs_mar.sh
@@ -1,20 +1,20 @@
 #!/bin/bash
-#SBATCH --gpus-per-node=1
+#SBATCH --gpus-per-node=4
 #SBATCH --job-name=mar-2xs-1k
 #SBATCH --time=06:00:00
-#SBATCH --output=/work/hdd/bdta/aqian1/mar_ebwm/logs/slurm_outputs/mar-2xs-%j.out
+#SBATCH --output=/work/hdd/bdta/aqian1/mar_ebwm_coding/logs/slurm_outputs/mar-2xs-%j.out
 
 source activate mar
 cd /work/hdd/bdta/aqian1/mar_ebwm
 
-torchrun --nproc_per_node=1 --nnodes=1 --node_rank=0 --master_addr=localhost --master_port=4738 main_mar.py \
+torchrun --nproc_per_node=4 --nnodes=1 --node_rank=0 --master_addr=localhost --master_port=4738 main_mar.py \
 --run_name mar-2xs-1k \
 --img_size 256 \
 --vae_path pretrained_models/vae/kl16.ckpt \
 --vae_embed_dim 16 --vae_stride 16 --patch_size 1 \
 --model mar_2xs --model_type mar \
 --diffloss_d 3 --diffloss_w 1024 \
---epochs 50 --warmup_epochs 5 \
+--epochs 400 --warmup_epochs 100 \
 --batch_size 64 --blr 5e-5\
  --diffusion_batch_mul 4 \
 --output_dir ./output/mar-2xs-1k \
--- submit_job.sh
+++ submit_job.sh
@@ -1,1 +1,4 @@
 bash slurm/slurm_exec.sh ncsa_a100 slurm/job_configs/train_2xs_mar.sh
+bash slurm/slurm_exec.sh ncsa_a100 slurm/job_configs/train_mar_base.sh
+bash slurm/slurm_exec.sh ncsa_a100 slurm/job_configs/train_mar_large.sh
+bash slurm/slurm_exec.sh ncsa_a100 slurm/job_configs/train_mar_huge.sh
